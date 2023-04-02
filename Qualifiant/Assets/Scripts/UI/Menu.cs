99b9e858a1f4af2b3d21d323ceae0edc8142bf0a
leo BG <leobgely@gmail.com>
2023-04-02 14:24:17 +0200

    feat(ui): add consign

---
+++ Qualifiant/Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class Menu : MonoBehaviour
+    {
+        [SerializeField] private Transform root;
+        
+        public bool isOpen => root.gameObject.activeSelf;
+        
+        private void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.Escape) || Input.GetKeyUp(KeyCode.Space))
+            {
+                ToggleMenu();
+            }
+        }
+
+        public void ToggleMenu()
+        {
+            root.gameObject.SetActive(!root.gameObject.activeSelf);
+        }
+
+        public void Retry()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        public void QuiteApp()
+        {
+            Application.Quit();
+        }
+    }
+}
